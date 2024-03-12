13f65640273b28cf06d68377e7415f4b2f29eeec
flynn <ssc22223@gmail.com>
2024-03-12 22:00:55 +0800

    基本业务完成

--- TwoWayMerchant/settings.py
+++ TwoWayMerchant/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#settings.py末尾
-try:
-    from .local_setting import *
-except ImportError:
-    print(ImportError)
+#
+# #settings.py末尾
+# try:
+#     from .local_setting import *
+# except ImportError:
+#     print(ImportError)
